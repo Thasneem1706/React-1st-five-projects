11a0e17c58630b28a11af4ffbda348290ab79a88
Thasneem Parveen <dev1@easylinkindia.com>
2024-06-26 13:10:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Accordion from './components/accordion';
+import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Accordion />
+      <RandomColor /> 
+      <StarRating noOfStars={10}/> 
+      <ImageSlider  url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/> 
+      <LoadMoreData />
+      
     </div>
   );
 }
