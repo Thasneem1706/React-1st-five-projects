612f6a851006fc12399c653f7807f0ffa8ea55b7
Thasneem Parveen <dev1@easylinkindia.com>
2024-07-02 15:51:18 +0530

    custom tabs, popup, github profile finder, search autocomplete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
-import LightDarkMode from './components/light-dark-mode';
-import QRCodeGenerator from './components/qr-code-generator';
-import ScrollIndicator from './components/scroll-indicator';
+import ModalTest from './components/custom-modal-popup/modal-test';
+import TabTest from './components/custom-tabs/tab-test';
+import GithubProfileFinder from './components/github-profile-finder';
+import SearchAutocomplete from './components/search-autocomplete-withapi';
+// import LightDarkMode from './components/light-dark-mode';
+// import QRCodeGenerator from './components/qr-code-generator';
+// import ScrollIndicator from './components/scroll-indicator';
 // import Accordion from './components/accordion';
 // import ImageSlider from './components/image-slider';
 // import LoadMoreData from './components/load-more-data';
 // import RandomColor from './components/random-color';
 // import StarRating from './components/star-rating';
-import TreeView from './components/tree-view';
-import menus from './components/tree-view/data';
+// import TreeView from './components/tree-view';
+// import menus from './components/tree-view/data';
 
 function App() {
   return (
@@ -21,10 +25,14 @@ function App() {
       <ImageSlider  url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/> 
       <LoadMoreData /> */}
 
-      <TreeView menus={menus}/> 
+      {/* <TreeView menus={menus}/> 
       <QRCodeGenerator /> 
       <LightDarkMode />
-      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/>
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/> */}
+      <TabTest />
+      <ModalTest />
+      <GithubProfileFinder />
+      <SearchAutocomplete />
       
     </div>
   );
---
+++ src/components/github-profile-finder/index.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import User from "./user"
+import './styles.css'
+
+
+
+export default function GithubProfileFinder(){
+
+    const [userName , setUserName]= useState('Thasneem1706')
+    const [userData, setUserData] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    async function fetchGithubUserData(){
+        setLoading(true)
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json()
+        if(data){
+            setUserData(data)
+            setLoading(false)
+            setUserName('')
+        }
+
+        // console.log(data)
+    }
+
+    function handleSubmit(){
+        fetchGithubUserData()
+
+    }
+    useEffect(()=>{
+        fetchGithubUserData()
+    },[])
+    if(loading){
+        return <h1>Loading data ! please wait</h1>
+    }
+    return(
+        <div className="github-profile-container">
+            <div className="input-wrapper">
+                <input type="text"
+                name="search-by-username"
+                placeholder="Search Github Username..."
+                value={userName}
+                onChange={(event) => setUserName(event.target.value)}
+                />
+                <button onClick={handleSubmit}>Search</button>
+
+            </div>
+            {userData !== null ? <User user={userData} />: null }
+
+        </div>
+        
+    )
+}
